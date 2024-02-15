6f7c92762312660d988ab02417e6c02e7a93da7c
Suraj Kumar <navneetraj07903@gmail.com>
2024-02-15 23:42:35 +0530

    First commit for Backup src folder

--- Backup Src/src/app/app.component.spec.ts
+++ Backup Src/src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
     declarations: [AppComponent]
   }));
 
@@ -12,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project1'`, () => {
+  it(`should have as title 'main_project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project1');
+    expect(app.title).toEqual('main_project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('project1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('main_project app is running!');
   });
 });
